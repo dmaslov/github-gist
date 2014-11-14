25fd3830bb9ca0fba340c2765b9654e4671c4f87
Dmitry Maslov <maslov.dmitrij@gmail.com>
2014-11-14 02:33:34 +0200

    Material design added

---
+++ control.js
@@ -0,0 +1,46 @@
+(function(){
+  'use strict';
+
+  var stylesheets = {
+    monokai: 'bower_components/github-gist/themes/monokai.css',
+    github: ''
+  };
+
+  var githubGist = document.querySelector('github-gist'),
+      paperInput = document.querySelector('paper-input'),
+      btnWrapper = document.querySelector('.theme'),
+      btns = btnWrapper.querySelectorAll('paper-button'),
+      timeout;
+
+  function eventHandler(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    var activeBtn = e.target,
+        activeBtnId = e.target.id;
+
+    githubGist.setAttribute('stylesheet', stylesheets[activeBtnId]);
+
+    for(var i in btns){
+      if(btns.hasOwnProperty(i) && typeof btns[i] === 'object'){
+        btns[i].classList.remove('active');
+      }
+    }
+
+    activeBtn.classList.add('active');
+  }
+
+  btnWrapper.addEventListener("click", eventHandler, false);
+  paperInput.addEventListener("input", function(e){
+    e.stopPropagation();
+    e.preventDefault();
+    if(this.value){
+      if(typeof timeout !== 'undefined'){
+        clearTimeout(timeout);
+      }
+      timeout = setTimeout(function(){
+        githubGist.setAttribute('gistid', this.value);
+      }.bind(this), 350);
+    }
+  });
+})();
